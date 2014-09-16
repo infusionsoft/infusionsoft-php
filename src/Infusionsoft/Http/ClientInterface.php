4fcd8cf34e52d90e61e53cffcced382455bbfcf5
Andrew Ryno <andrewryno@gmail.com>
2014-09-16 08:58:52 -0700

    Add back request methods.

--- src/Infusionsoft/Http/ClientInterface.php
+++ src/Infusionsoft/Http/ClientInterface.php
@@ -9,4 +9,15 @@ interface ClientInterface {
 	 */
 	public function getXmlRpcTransport();
 
+	/**
+	 * Sends a request to the given URI and returns the raw response.
+	 *
+	 * @param string $uri
+	 * @param array  $params
+	 * @param array  $headers
+	 * @param string $method
+	 * @return mixed
+	 */
+	public function request($uri, $params, $headers, $method);
+
 }
--- src/Infusionsoft/Http/CurlClient.php
+++ src/Infusionsoft/Http/CurlClient.php
@@ -26,4 +26,60 @@ class CurlClient extends CurlTransport implements ClientInterface {
 		return parent::send($uri, $payload);
 	}
 
+	/**
+	 * @param string $uri
+	 * @param array  $body
+	 * @param array  $headers
+	 * @param string $method
+	 * @return mixed
+	 * @throws HttpException
+	 */
+	public function request($uri, $body, $headers, $method)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $uri);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cacert.pem');
+
+		if ($method === 'POST')
+		{
+			curl_setopt($ch, CURLOPT_POST, true);
+
+			if ($body && is_array($body))
+			{
+				$body = http_build_query($body, '', '&');
+			}
+
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+		}
+		else
+		{
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+		}
+
+		$response     = curl_exec($ch);
+		$responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if (false === $response)
+		{
+			$errNo  = curl_errno($ch);
+			$errStr = curl_error($ch);
+			curl_close($ch);
+
+			if (empty($errStr))
+			{
+				throw new HttpException('There was a problem requesting the resource.', $responseCode);
+			}
+
+			throw new HttpException($errStr . '(cURL Error: ' . $errNo . ')', $responseCode);
+		}
+
+		curl_close($ch);
+
+		return json_decode($response, true);
+	}
+
 }
