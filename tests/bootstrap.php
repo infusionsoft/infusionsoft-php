e611048c77724b12f45793cffeac9b8fcee18e8d
Michael Fairchild <michael@mfairchildblog.com>
2015-01-29 15:23:06 -0700

    Added Date Formating function and fixed unit test to have a default timezone set.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
+date_default_timezone_set("America/New_York");
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/ServiceTest.php';
