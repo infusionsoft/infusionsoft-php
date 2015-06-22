ce77788a49df400bedbefa1e59c2984e19cd767a
micfai <michael.fairchild@infusionsoft.com>
2015-06-22 13:49:40 -0700

    Fix how Curl driver handles headers

--- src/Infusionsoft/Http/CurlClient.php
+++ src/Infusionsoft/Http/CurlClient.php
@@ -36,30 +36,34 @@ class CurlClient extends CurlTransport implements ClientInterface {
 	 */
 	public function request($uri, $body, $headers, $method)
 	{
+		$processed_headers = array();
+		if(!empty($headers))
+		{
+			foreach($headers as $key => $value)
+			{
+				$processed_headers[] = $key . ': ' . $value;
+			}
+		}
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $uri);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $processed_headers);
 		curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cacert.pem');
-
 		if ($method === 'POST')
 		{
 			curl_setopt($ch, CURLOPT_POST, true);
-
 			if ($body && is_array($body))
 			{
 				$body = http_build_query($body, '', '&');
 			}
-
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
 		}
 		else
 		{
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
 		}
-
 		$response     = curl_exec($ch);
 		$responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
