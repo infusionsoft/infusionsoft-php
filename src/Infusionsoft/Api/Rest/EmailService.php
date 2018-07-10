46c3d58ac19030258c8cc105a44bf6e23ef74e64
M Fairchild <2048552+mfairch@users.noreply.github.com>
2018-07-10 06:56:51 -0700

    Fixes #170

--- src/Infusionsoft/Api/Rest/EmailService.php
+++ src/Infusionsoft/Api/Rest/EmailService.php
@@ -1,14 +1,13 @@
 <?php namespace Infusionsoft\Api\Rest;
 
 use Infusionsoft\Api\Rest\Traits\CannotDelete;
-use Infusionsoft\Api\Rest\Traits\CannotFind;
 use Infusionsoft\Api\Rest\Traits\CannotModel;
 use Infusionsoft\Api\Rest\Traits\CannotSave;
 use Infusionsoft\Api\Rest\Traits\CannotSync;
 
 class EmailService extends RestModel
 {
-    use CannotSync, CannotSave, CannotDelete, CannotFind, CannotModel;
+    use CannotSync, CannotSave, CannotDelete, CannotModel;
 
     public $full_url = 'https://api.infusionsoft.com/crm/rest/v1/emails';
 
