16855b7b4c2c4168a4911255cc1b944f11f5e113
Michael Fairchild <michael.fairchild@infusionsoft.com>
2019-02-21 08:46:21 -0700

    add rest order payment method

--- src/Infusionsoft/Api/Rest/OrderService.php
+++ src/Infusionsoft/Api/Rest/OrderService.php
@@ -9,4 +9,13 @@ class OrderService extends RestModel {
 	public $full_url = 'https://api.infusionsoft.com/crm/rest/v1/orders';
 	public $return_key = 'orders';
 
+
+    public function addPayment($paymentDetails)
+    {
+
+        $response = $this->client->restfulRequest('post', $this->getFullUrl($this->id . '/payments'), $paymentDetails);
+
+        return $response;
+    }
+
 }
