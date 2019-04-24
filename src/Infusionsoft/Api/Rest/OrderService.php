93a30fea764f7b925de2d8d40b34a612c284bf9c
Gerd R <gerd.roensch@icloud.com>
2019-04-24 17:21:50 +0200

    Enable the delete method for the orders (REST) (#214)
    There is definitely an option to delete the order via the API and after testing I found out that just by removing that trait it is possible to delete orders successfully.

--- src/Infusionsoft/Api/Rest/OrderService.php
+++ src/Infusionsoft/Api/Rest/OrderService.php
@@ -4,8 +4,6 @@ use Infusionsoft\Api\Rest\Traits\CannotDelete;
 
 class OrderService extends RestModel {
 
-	use CannotDelete;
-
 	public $full_url = 'https://api.infusionsoft.com/crm/rest/v1/orders';
 	public $return_key = 'orders';
 
