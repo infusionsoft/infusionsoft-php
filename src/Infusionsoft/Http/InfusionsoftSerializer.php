3e798dfecd4aa75ee3652ce0b28e9703183ac2b8
Andrew Donald Johnson <ajohnson6494@gmail.com>
2020-03-03 09:14:50 -0500

    Use NativeParser if xmlrpc is loaded.

--- src/Infusionsoft/Http/InfusionsoftSerializer.php
+++ src/Infusionsoft/Http/InfusionsoftSerializer.php
@@ -4,35 +4,42 @@ namespace Infusionsoft\Http;
 
 use fXmlRpc\Client;
 use fXmlRpc\Exception\ExceptionInterface as fXmlRpcException;
+use fXmlRpc\Parser\BestParserDelegate;
 
 class InfusionsoftSerializer implements SerializerInterface {
 
-	/**
-	 * @param string          $method
-	 * @param string          $uri
-	 * @param array           $params
-	 * @param ClientInterface $client
-	 * @return mixed|void
-	 * @throws HttpException
-	 */
-	public function request($method, $uri, $params, ClientInterface $client)
-	{
-		// Although we are using fXmlRpc to handle the XML-RPC formatting, we
-		// can still use Guzzle as our HTTP client which is much more robust.
-		try
-		{
-			$transport = $client->getXmlRpcTransport();
+  /**
+   * @param string          $method
+   * @param string          $uri
+   * @param array           $params
+   * @param ClientInterface $client
+   * @return mixed|void
+   * @throws HttpException
+   */
+  public function request($method, $uri, $params, ClientInterface $client)
+  {
+    // Although we are using fXmlRpc to handle the XML-RPC formatting, we
+    // can still use Guzzle as our HTTP client which is much more robust.
+    try
+    {
+      if(extension_loaded('xmlrpc')) {
+        $parser = new BestParserDelegate();
+      } else {
+        $parser = null;
+      }
 
-			$client = new Client($uri, $transport);
+      $transport = $client->getXmlRpcTransport();
 
-			$response = $client->call($method, $params);
+      $client = new Client($uri, $transport, $parser);
 
-			return $response;
-		}
-		catch (fXmlRpcException $e)
-		{
-			throw new HttpException($e->getMessage(), $e->getCode(), $e);
-		}
-	}
+      $response = $client->call($method, $params);
+
+      return $response;
+    }
+    catch (fXmlRpcException $e)
+    {
+      throw new HttpException($e->getMessage(), $e->getCode(), $e);
+    }
+  }
 
 }
